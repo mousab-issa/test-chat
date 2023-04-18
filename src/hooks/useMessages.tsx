4ab743059b6b20c3a30c5608c7bc889a44fa3f4f
mousab-issa <59908473+mousab-issa@users.noreply.github.com>
2023-04-18 14:17:45 +0700

    feat: adding channel id

--- src/hooks/useMessages.tsx
+++ src/hooks/useMessages.tsx
@@ -1,20 +1,32 @@
 import { useQuery } from "@apollo/client";
 import React, { useContext } from "react";
 import ChannelContext from "../context/ChannelContext";
-import MessageContext from "../context/MessageContext";
+import MessageContext, { Message } from "../context/MessageContext";
 import { FETCH_LATEST_MESSAGES } from "../graphQl/queries";
 
 export const useMessages = () => {
   const { messages, dispatch } = useContext(MessageContext);
   const { currentChannel } = useContext(ChannelContext);
 
-  const { data } = useQuery(FETCH_LATEST_MESSAGES, {
-    variables: { channelId: currentChannel?.id },
-  });
+  const { data } = useQuery<{ fetchLatestMessages: Message[] }>(
+    FETCH_LATEST_MESSAGES,
+    {
+      variables: { channelId: currentChannel?.id },
+    }
+  );
 
   React.useEffect(() => {
     if (data) {
-      dispatch({ type: "SET_MESSAGES", payload: data.fetchLatestMessages });
+      dispatch({
+        type: "SET_MESSAGES",
+        payload: data.fetchLatestMessages.map((message) => {
+          if (!currentChannel?.id) {
+            return message;
+          }
+
+          return { ...message, channelId: currentChannel.id, error: false };
+        }),
+      });
     }
   }, [data, dispatch]);
 
